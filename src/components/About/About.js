33a0ecbfde419e885fe976b302ec3f997b6e5b9c
SHYAKA Benjamin <59408657+benshyaka@users.noreply.github.com>
2022-08-12 13:57:16 +0200

    - adding project and their logos

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,34 +7,42 @@ export const About = () => {
   return (
     <section className="container container-about">
       <div className="about-left">
-        <span className="purple-text">
-          ✋Hello again! call me Ben{" "}
-        </span>
+        <h5>
+          <span className="purple-text">
+            ✋Hello again! You can call me Ben{" "}
+          </span>
+        </h5>
+        
         <p className="gray-text">
           I am a young full stack developer based in Rwanda. I build websites, {" "} <br></br>
           I am a quick learner who is continually adapting to new technologies; 
           I am familiar with javascript and its most well-known frameworks.
           I'm obsessed with pixel-perfect, gorgeous interfaces and well-implemented UX.
         </p>
-        <a className="" target={"_blank"} rel="noreferrer" href="https://docs.google.com/document/d/1UdbNbu-poO-ep-rzcxXc6NgYEoKfFiAi/edit#">Take a look at my CV {"->"} </a>
-        <a className="" target={"_blank"} rel="noreferrer" href="https://github.com/benshyaka">
-          <img className="img-fluid" width={24} src={githublogo} alt="abajo" /> {"  "} Visit my github
-        </a>
-        <span className="white-text">
-          <h5>Services i provide</h5>
-        </span>
-        <div className="row d-flex">
-          <div className="card col">
+        <div className="d-grid gap-3 d-md-block">
+          <a className="btn btn-outline-primary btn-sm me-0 me-sm-3" target={"_blank"} rel="noreferrer" href="https://docs.google.com/document/d/1UdbNbu-poO-ep-rzcxXc6NgYEoKfFiAi/edit#">Take a look at my CV</a>
+          <a className="btn btn-outline-primary btn-sm" target={"_blank"} rel="noreferrer" href="https://github.com/benshyaka">
+            <img className="img-fluid me-3" width={22} src={githublogo} alt="abajo" /> {"  "} Visit my github
+          </a>
+        </div>
+        
+          <h5 className="pt-3">
+            <span className="purple-text">
+              Services i provide
+            </span>
+          </h5>
+
+        <div className="row">
+          <div className="card col-11 col-lg-4">
             <div className="card-body">
               <h5 className="card-title">Web development</h5>
               <h6 className="card-subtitle mb-2 text-muted ">Any website type</h6>
               <p className="card-text">
                 I design proffesional quality websites with 24/7 security monitoring and hosting
               </p>
-              
             </div>
           </div>
-          <div className="card col">
+          <div className="card col-11 col-lg-3">
             <div className="card-body">
               <h5 className="card-title">UI/UX Design</h5>
               <h6 className="card-subtitle mb-2 text-muted ">Any design type</h6>
@@ -44,7 +52,7 @@ export const About = () => {
               
             </div>
           </div>
-          <div className="card col">
+          <div className="card col-11 col-lg-3">
             <div className="card-body">
               <h5 className="card-title">Digital marketing</h5>
               <h6 className="card-subtitle mb-2 text-muted ">Any social medias</h6>
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,46 +1,83 @@
 import React from "react";
 import "./Project.css";
-import Line from "../../images/line-purple.svg";
-import ImageTemp from "../../images/prev-project-temp.png";
-import FigmaIcon from "../../images/figma-icon.svg";
+import utickets from "../../assets/utickets.PNG"
+import kasa from "../../assets/kasa.png"
+import humeeka from "../../assets/humeeka.PNG"
 // import githublogo from "../../images/github.png"
 
 function Project() {
   return (
     <section className="container container-project">
+      <h5 className="pb-3">
+            <span className="purple-text">
+              My projects
+            </span>
+          </h5>
       <p className="purple-text">
-        ‍💻 Open Source -
-        <span className="purple-smoke-text">Visita mis ultimos proyectos </span>{" "}
+        ‍💻 My Open Source Projects - On Github
       </p>
-      <div className="project-header">
-        <div className="project-box">
-          <span className="white-text">react-dark-portfolio</span>
-          <p className="gray-text text-box">
-            Templete para que muestres tus proyectos como un portafolio usando
-            reactjs
-          </p>
-        </div>
-        <img className="project-line" src={Line} alt="abajo" />
+      <div className="row">
+          <div className="card col-12 col-lg-3">
+            <div className="project-box">
+            <img src={utickets} alt="Utickets" className="img-fluid"/> <br></br>
+              <span className="white-text pt-5">UTickets</span>
+              <p className="gray-text text-box">
+                Event ticketing app, it is build using Javascript technologies like NodeJs, ExpressJs, EJS and Postgress
+              </p>
+              <p>
+                <a className="" target={"_blank"} rel="noreferrer" href="https://ubumweapp.herokuapp.com/">Link to UTickets</a>
+              </p>
+            </div>
+          </div>
+
+          <div className="card col-12 col-lg-3">
+            <div className="project-box">
+            <img src={kasa} alt="Utickets" className="img-fluid"/> <br></br>
+              <span className="white-text">Kasa</span>
+              <p className="gray-text text-box">
+                I developed the online plants seeds selling system for the startup called Kasa.
+              </p>
+              <p>
+                <a className="" target={"_blank"} rel="noreferrer" href="https://www.kasa.rw">Link to Kasa.rw</a>
+              </p>
+            </div>
+          </div>
+
+          <div className="card col-12 col-lg-3">
+            <div className="project-box">
+            <img src={humeeka} alt="Utickets" className="img-fluid"/> <br></br>
+              <span className="white-text">Humeeka</span>
+              <p className="gray-text text-box">
+                I am the CEO and the Co-founder of a startup company Humeeka, it is an IOT Based application that monitors indoor air quality in realtime.
+              </p>
+              <p>
+                <a className="" target={"_blank"} rel="noreferrer" href="https://humeeka.com">Link to Humeeka.com</a>
+              </p>
+            </div>
+          </div>
       </div>
+      
+
       <div className="project-details">
-        <img src={ImageTemp} alt="Project React Simple Portfolio" />
+        {/* <img src={ImageTemp} alt="Project React Simple Portfolio" /> */}
         <div className="details-left">
-          <p className="gray-text">
+          {/* <p className="gray-text">
             Mira el diseño de este proyecto en
             <span className="text-figma">
               <img className="icon-figma" src={FigmaIcon} alt="" />
               Figma
             </span>
-          </p>
+          </p> */}s
           <div className="details-stack">
-            <span className="white-text">Tecnologias</span>
+            <span className="white-text">Skills and technologies</span>
             <ul className="gray-text">
-              <li>Reactjs</li>
-              <li>CMS (Contenful)</li>
-              <li>Nodejs</li>
+              <li><b>Front-end Languages and Frameworks:</b>  HTML, CSS, JavaScript, React and JQuery</li>
+              <li><b>Backend Technologies and Frameworks:</b>  JavaScript, NodeJS and ExpressJS</li>
+              <li><b>Database Management Systems:</b>  MySQL, SQL SERVER and PostgreSQL, MongoDB</li>
+              <li><b>Web Hosting Platforms:</b>  AWS, Google cloud platform, Heroku, and Microsoft Azure</li>
             </ul>
           </div>
-          <p className="gray-text">Una estrellita en <span className="white-text">Github</span> y mira en vivo la <span className="white-text">Web</span> </p>
+          <p className=""> <span className="white-text">I also have Git and GitHub skills, as well as communication and teamworking.</span></p>
 
         </div>
       </div>
